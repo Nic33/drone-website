d306fa070829c99f0446c6d752d9861a98b14ea8
Nicolas Dubuisson <nicolas.dubuisson@etu.u-bordeaux.fr>
2022-06-26 22:42:07 +0200

    maj

---
+++ src/components/Navbar.js
@@ -0,0 +1,62 @@
+import React,{useState} from 'react'
+import {Link} from 'react-router-dom';
+import './Navbar.css';
+import logo from "./logo.png";
+import { Button } from './Button';
+
+function Navbar() {
+    const [click,setClick] = useState(false);
+
+    const handleClick = () => setClick(!click)
+    const closeMobileMenu = () => setClick(false);
+/* 
+    const [button,setButton] = useState(true);
+
+    const showButton = () => {
+        if (window.innerWidth <= 960){
+            setButton(true);
+        }else{
+            setButton(true);
+        }
+    };
+
+    window.addEventListener('resize', showButton);
+ */
+    return (
+        <nav className="navbar">
+
+            <Link to="/" className ="navbar-logo">
+                TRVL  <img src={logo}/>
+            </Link>
+
+            <div className="menu-bar" onClick={handleClick}>
+                <span className = "material-symbols-outlined">
+                    {click? "close": "menu"}
+                </span>
+            </div>
+
+            <ul className = {click? "nav-menu active":"nav-menu"}>
+
+                <Link to = "/" className="nav-link" onClick={closeMobileMenu}>
+                    Home
+                </Link>
+                
+                <Link to = "/Components" className="nav-link" onClick={closeMobileMenu}>
+                    Components
+                </Link>
+
+                <Link to = "/Software" className="nav-link" onClick={closeMobileMenu}>
+                    Software
+                </Link>
+
+                <Link to = "/Important" className="nav-link" onClick={closeMobileMenu}>
+                    Important
+                </Link>
+
+                {/* button && */ <Button buttonStyle="btn--outline">SIGN UP</Button>}
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar
